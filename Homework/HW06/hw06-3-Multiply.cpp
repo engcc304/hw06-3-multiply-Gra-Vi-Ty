c1e12b9dfbce876cfac0caabcdeea5b505907866
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-11 01:42:17 +0700

    uppp

--- Homework/HW06/hw06-3-Multiply.cpp
+++ Homework/HW06/hw06-3-Multiply.cpp
@@ -88,9 +88,9 @@ int main(){
 
     for( int i = start; i <= end; i++) {
         for (int j = 1; j <= 9; j++) {
-            printf("%d x %d = %d\n", i, j, i * j);
+            printf( "%d x %d = %d\n", i, j, i * j) ;
         }
-        printf("\n");
+        printf( "\n" ) ;
     }
 
     return 0 ;
